940522d8709a322caade3e83e2b6052e3d41f60e
Oz <osman.ozdemir@adp.com>
2016-06-13 11:30:06 -0400

    utility functions exposed

--- src/lib/index.js
+++ src/lib/index.js
@@ -3,11 +3,13 @@ import Runner from './runner';
 import Parser from './parser';
 import PluginManager from './plugin-manager';
 import Plugin from './plugins/plugin';
+import Utility from './utility';
 
 module.exports = {
   Tester,
   Runner,
   Parser,
   PluginManager,
-  Plugin
+  Plugin,
+  Utility
 }
